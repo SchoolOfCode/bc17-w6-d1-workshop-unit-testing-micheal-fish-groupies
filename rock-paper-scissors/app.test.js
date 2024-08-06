961fc7fb4e9da1a44cdab53f05ad0045ba8c17a3
Ian Smythe <ianbsmythe@googlemail.com>
2024-08-06 10:36:45 +0100

    Ticket 5 - setting up tests on app.test.js

---
+++ rock-paper-scissors/app.test.js
@@ -0,0 +1,21 @@
+//rename app.txt to app.js Y
+//use prettier to format the code Y
+// look through app.js for functions Y
+// create an app.test.js file Y
+
+// import the functions from app.js Y
+import { calculateRoundResult, calculateNewScores, generateComputerMove } from './app';
+
+// create a bare bones test
+import { test, expect } from 'vitest';
+
+test('bare bones test', () => {
+  expect(true).toBe(true);
+});
+
+// write test cases/scenarios for calculateRoundResult
+test('bare bones test', () => {
+    expect(true).toBe(true);
+  });
+  
+
